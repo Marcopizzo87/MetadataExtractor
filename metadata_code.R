e30550738e1404837966bddfeaf099a5f0145d4b
Marcopizzo87 <pizzolatomarco@yahoo.it>
2019-02-16 23:23:24 +0000

    Mo basta

--- metadata_code.R
+++ metadata_code.R
@@ -1,57 +1,81 @@
 #####################################################
 #       Metadata Extractor from shapefile           #
 #                                                   #  
-#                November 22th 2018                 #
+#                February 16th 2019                 #
 #                 Marco Pizzolato                   #
 #####################################################
 
 ### SHORT DESCRIPTION ----
 
-# The script loops through the shapefile in a folder and creates a report 
+# The script loops through the shapefile in a folder and creates a report.
 
 ### PACKAGES ----
 
 ## Install previous version of rmarkdown
-#install.packages("devtools")
-#library(devtools)
-#install_version("rmarkdown",version=1.8)
+# install.packages("devtools")
+# library(devtools)
+# install_version("rmarkdown",version=1.8)
+
+install.packages('D:/Downloads/shapefiles_0.7.zip', repos = NULL, type = "win.binary")
+
+library(sf) # manipulate shp
+library(shapefiles) # library used to get the attr table
+
+install.packages("shapefile")
 
 library(devtools)
 library(rmarkdown) # ensure to have version 1.8
 library(tinytex) 
-library(sf) # manipulate shp
-library(xlsx) # read xls
+
+
 library(shapefiles) # library used to get the attr table
 library(knitr) # create table in Rmd file
 
 ### SET WORKING DIRECTORY ----
 
 # get path to current folder
-path <- dirname(rstudioapi::getSourceEditorContext()$path)
-path2 <- path
+# path <- dirname(rstudioapi::getSourceEditorContext()$path)
+# path2 <- path
 # get path to current folder + file
-#path3 <- rstudioapi::getSourceEditorContext()$path
-# set the WD to the current path
+# path3 <- rstudioapi::getSourceEditorContext()$path
 
-setwd(path)
+# path <- ("D:/Desktop/SSD_GIS/Flood_Map/Awerial")
+
+# set the WD to the current path
+# setwd(path)
+
+
+
+###  SET THESE PARAMETERS ----
+
+# folder you want to scan (the scrip scans also the sub-folders)
+path_dest <- ("D:/Desktop/SSD_GIS/Flood_Map")
+
+
+###  SCRIPT ----
+
+# Set WD automatically
+setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 
 # Check the wd
-#getwd()
+getwd()
 
 # Clean the environment
-#rm(list = ls())
+# rm(list = ls())
+
 
 
 ### READ EXCEL ----
-# see if the file exists in the directory
-exist <- which(list.files() == "metadata_to_fill.xlsx")
+# see if in the directory I want to scan there is an excel file with the general informations for the report
+exist <- which(list.files(path_dest) == "metadata_to_fill.csv")
 
 
-# if hte file does not exists fills with "not available data"
+# if the excel file is not available fills the relative information with "not available data"
 if (!length(exist) == 0){
-  df <- read.xlsx(paste(path,"/metadata_to_fill.xlsx", sep=""), sheetIndex = 1, header=FALSE, encoding="UTF-8")
+  df <- read.csv(paste(path_dest,"/metadata_to_fill.csv", sep=""), header=FALSE, encoding="Latin-1")
 } else {
   df <- data.frame(stringsAsFactors=FALSE)
+  # author
   df[1,1] <- "Auteur(s): "
   df[1,2] <- "not available "
   # geometry
@@ -71,21 +95,22 @@ if (!length(exist) == 0){
 
 ### READ THE SHAPEFILE ----
 
-setwd("..")
+list_shp <- list.files(path = path_dest, pattern = ".*shp$", recursive = TRUE) # gets all the shapefile in the folder
 
-path_disp <- as.data.frame(path)
-
-
-list_shp <- list.files(path = ".", pattern = ".*shp$")
 list_whaaat <- list()
+
+# this is the DF that hosts all the metadata information
 tabellone <- data.frame(stringsAsFactors=FALSE)
+
+# this is the list of all the "tabellone" dataframes belonging to each shapefile
 list_tabellone <- list()
+
 
 for (i in 1:length(list_shp)){
   
   # read shapefile with sf package
-  att_table <- st_read(paste("./",list_shp[i],sep = ""))
-  att_layers <- st_layers(paste("./",list_shp[i],sep = ""))
+  att_table <- st_read(paste(path_dest,"/",list_shp[i],sep = ""))
+  att_layers <- st_layers(paste(path_dest,"/",list_shp[i],sep = ""))
   
   # get the attribute table compiled
   # name
@@ -120,6 +145,9 @@ for (i in 1:length(list_shp)){
 
 
 ### PRINT THE PDF ----
+
+
+path_print <- as.data.frame(path_dest) # save the file in df for the markdown
 
 rmarkdown::render( input = "metadataExtractor/metadata.Rmd",
                    output_format = "pdf_document",
